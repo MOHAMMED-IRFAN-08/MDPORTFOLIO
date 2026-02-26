6452be46446267a3c5b58090326816e78ae556e5
MOHAMMED-IRFAN-08 <mdrirfan4453@gmail.com>
2026-02-26 16:34:07 +0530

    first portfolio commit

---
+++ Sample.js
@@ -0,0 +1,9 @@
+var a =20;
+console.log(a);
+console.error(a);
+console.warn(a);
+function changeText(){
+    document.getElementById("demo").innerHTML="<h1>Hello JavaScript</h1>";
+    document.getElementById("demo123").innerText="<h1>Hello JavaScript</h1>";
+    
+}
